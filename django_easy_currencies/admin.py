1da1d8e3f411c0c2aa10aee29069988ac40f7bae
davidezanotti <davide.zanotti@cygora.com>
2014-11-17 17:15:06 +0100

    fixed django admin registration + better CurrencyRateAdmin class

--- django_easy_currencies/admin.py
+++ django_easy_currencies/admin.py
@@ -3,5 +3,10 @@ from django.contrib import admin
 from models import Currency, CurrencyRate
 
 
-admin.register(Currency)
-admin.register(CurrencyRate)
+class CurrencyRateAdmin(admin.ModelAdmin):
+    list_display = ('original_currency', 'target_currency', 'rate')
+    list_filter = ('original_currency', 'target_currency')
+
+
+admin.site.register(Currency)
+admin.site.register(CurrencyRate, CurrencyRateAdmin)
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-easy-currencies',
-    version='0.2.0',
+    version='0.2.1',
     packages=['django_easy_currencies'],
     install_requires=['django', 'Babel>=1.3'],
     include_package_data=True,
